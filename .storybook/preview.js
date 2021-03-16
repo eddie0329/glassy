5899e971a3739810bcd070ad2d80ad6d06785677
eddie0329 <fdsa0329@naver.com>
2021-03-16 23:22:58 +0900

    :wrench: Change storybook preview config

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -5,8 +5,8 @@ export const decorators = [
   () => {
     return {
       template: `
-      <div style="background: linear-gradient(90deg,#FFFF00 6%,#FFA500 25%,#F14444 45%,#D53567 55%,#9A109A 94%); height: 100vh; padding: 20px; box-sizing: border-box;">
-        <div style="background: rgba(255, 255, 255, 0.4); border-radius: 10px; border: 1px solid rgba(255, 255, 255, 0.2); backdrop-filter: blur(5px); -webkit-backdrop-filter: blur(5px); padding: 20px; height: 100%; box-sizing: border-box;">
+      <div style="background: linear-gradient(90deg,#FFFF00 6%,#FFA500 25%,#F14444 45%,#D53567 55%,#9A109A 94%); height: 100vh; padding: 20px; box-sz">
+        <div style="background: rgba(255, 255, 255, 0.4); border-radius: 10px; border: 1px solid rgba(255, 255, 255, 0.2); backdrop-filter: blur(5px); -webkit-backdrop-filter: blur(5px); padding: 20px; height: 100%; box-sizing: border-box; box-shadow: 0 0 100000px 100000px rgba(rgb(255,255,255), 0.2) inset;">
           <story />
         </div>
       </div>
