0bbeced7b107f716c6511683e0c38c427bf34c6a
eddie0329 <fdsa0329@naver.com>
2021-03-19 23:47:38 +0900

    master: :construction: Work in progress g-button

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -5,8 +5,8 @@ export const decorators = [
   () => {
     return {
       template: `
-      <div style="background: linear-gradient(90deg,#FFFF00 6%,#FFA500 25%,#F14444 45%,#D53567 55%,#9A109A 94%); height: 100vh; padding: 20px; box-sz">
-        <div style="background: rgba(255, 255, 255, 0.4); border-radius: 10px; border: 1px solid rgba(255, 255, 255, 0.2); backdrop-filter: blur(5px); -webkit-backdrop-filter: blur(5px); padding: 20px; height: 100%; box-sizing: border-box; box-shadow: 0 0 100000px 100000px rgba(rgb(255,255,255), 0.2) inset;">
+      <div style="height: 100vh; background: conic-gradient(from 180deg at 50% 50%, #EC4889 0deg, #F176B3 61.87deg, #A855F7 118.12deg, #6366F1 174.38deg, #3B82F6 232.5deg, #0EA5E9 294.38deg, #0686D4 360deg); border: 1px solid #000000; box-sizing: border-box;">
+        <div style="height: inherit; background: rgba(255, 255, 255, 0.2); backdrop-filter: blur(240px); padding: 20px; box-sizing: border-box;">
           <story />
         </div>
       </div>
---
+++ src/components/GButton.vue
@@ -0,0 +1,122 @@
+<template>
+  <button
+    ref="gButton"
+    g-button
+    @click="onClick"
+    :class="{ [color]: color, round }"
+  >
+    <slot></slot>
+    <div g-button-blur-border :class="{ round }"></div>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "GButton",
+  props: {
+    color: {
+      type: String,
+      default: "primary",
+    },
+    round: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    onClick() {
+      this.$emit("click");
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+@import "~@/less/asset";
+[g-button] {
+  .rel();
+  .ib();
+  font-family: "Poppins", sans-serif;
+  .h(38px);
+  .color(#ffffff);
+  .p(5px, 20px);
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  backdrop-filter: blur(100px);
+  &:hover {
+    opacity: 0.5;
+  }
+}
+
+[g-button-blur-border] {
+  .abs();
+  .l(-3.1);
+  .t(-3.1);
+  .ib();
+  .w(107%);
+  .h(38px);
+}
+
+.round {
+  border-radius: 8px;
+}
+
+.primary {
+  .bgc(@primary);
+  .-a(@primary, 3px);
+  [g-button-blur-border] {
+    .btn-shadow(@primary);
+  }
+}
+.secondary {
+  .bgc(@secondary);
+  .-a(@secondary, 3px);
+  [g-button-blur-border] {
+    .btn-shadow(@secondary);
+  }
+}
+.success {
+  .bgc(@success);
+  .-a(@success, 3px);
+  [g-button-blur-border] {
+    .btn-shadow(@success);
+  }
+}
+.danger {
+  .bgc(@danger);
+  .-a(@danger, 3px);
+  [g-button-blur-border] {
+    .btn-shadow(@danger);
+  }
+}
+.warning {
+  .bgc(@warning);
+  .-a(@warning, 3px);
+  [g-button-blur-border] {
+    .btn-shadow(@warning);
+  }
+}
+.info {
+  .bgc(@info);
+  .-a(@info, 3px);
+  [g-button-blur-border] {
+    .btn-shadow(@info);
+  }
+}
+.white {
+  .color(rgba(0, 0, 0, 0.25));
+  .bgc(@white);
+  .-a(@white, 3px);
+  [g-button-blur-border] {
+    .btn-shadow(@white);
+  }
+}
+.dark {
+  .bgc(@dark);
+  .-a(@dark, 3px);
+  [g-button-blur-border] {
+    .btn-shadow(@dark);
+  }
+}
+</style>
