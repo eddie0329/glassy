142d83f0756dc00269137d9881e373b02b83e6bd
eddie0329 <fdsa0329@naver.com>
2021-03-21 03:13:26 +0900

    master: :sparkles: Implement color constants

---
+++ src/constants/colors.js
@@ -0,0 +1,8 @@
+export const PRIMARY = "primary";
+export const SECONDARY = "secondary";
+export const SUCCESS = "success";
+export const DANGER = "danger";
+export const WARNING = "warning";
+export const INFO = "info";
+export const WHITE = "white";
+export const DARK = "dark";
